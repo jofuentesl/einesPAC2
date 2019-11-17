1d52b9368b2cd762894a62390b9c805913d95037
jofuentesle <jofuentesle@uoc.edu>
2019-11-17 07:10:53 +0100

    last update

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -118,7 +118,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"js/index.js":[function(require,module,exports) {
-console.log("hola mundo");
+lazyload();
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54815" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55818" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
